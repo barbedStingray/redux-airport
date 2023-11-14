e1d21e164391f9ffa5d6c1d4f92510baecb75826
Ben Rehmann <brehmann@me.com>
2023-11-14 16:44:45 -0600

    stretch complete

--- src/components/AirlineForm/AirlineForm.jsx
+++ src/components/AirlineForm/AirlineForm.jsx
@@ -5,25 +5,39 @@ import { useDispatch } from 'react-redux';
 export default function AirlineForm() {
 
     const [airlineName, setAirlineName] = useState('');
+    const [airplaneNumber, setAirplaneNumber] = useState('');
+
     const dispatch = useDispatch();
 
-// dispatch the airline
+    // dispatch the airline
     const addAirline = () => {
         console.log(`adding new airline...`);
 
-        let action = { type: 'ADD_AIRLINE', payload: airlineName }
+        let action = {
+            type: 'ADD_AIRLINE',
+            payload: { airlineName: airlineName, airplaneNumber: airplaneNumber }
+        }
         dispatch(action);
     }
 
 
     return (
         <div id="airline-form">
-            <input 
+            <input
                 value={airlineName}
                 onChange={(e) => setAirlineName(e.target.value)}
                 placeholder='Airline Name' />
-                {airlineName}
+            {airlineName}
+
+            <input
+                value={airplaneNumber}
+                type="number"
+                onChange={(e) => setAirplaneNumber(e.target.value)}
+                placeholder='Airplane Number' />
+            {airplaneNumber}
+
             <button onClick={addAirline} >Add Airline</button>
+
 
         </div>
     )
